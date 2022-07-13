0f5aa5a189d2de3d51e536b9111f43aa58f7684b
Jeron Aldaron Lau <aldaronlau@gmail.com>
2022-07-12 20:35:10 -0500

    Update licenses

--- README.md
+++ README.md
@@ -41,9 +41,9 @@ Rust) that depends on this crate.
 Licensed under any of
  - Apache License, Version 2.0, ([LICENSE_APACHE_2_0.txt][7]
    or [https://www.apache.org/licenses/LICENSE-2.0][8])
- - MIT License, ([LICENSE_MIT.txt][9] or [https://mit-license.org/][10])
  - Boost Software License, Version 1.0, ([LICENSE_BOOST_1_0.txt][11]
    or [https://www.boost.org/LICENSE_1_0.txt][12])
+ - MIT License, ([LICENSE_MIT.txt][9] or [https://mit-license.org/][10])
 
 at your option.
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,9 @@
-// WhoAmI
-// Copyright © 2017-2021 Jeron Aldaron Lau.
+// Copyright © 2017-2022 The WhoAmI Contributors.
 //
 // Licensed under any of:
-//  - Apache License, Version 2.0 (https://www.apache.org/licenses/LICENSE-2.0)
-//  - MIT License (https://mit-license.org/)
-//  - Boost Software License, Version 1.0 (https://www.boost.org/LICENSE_1_0.txt)
+// - Apache License, Version 2.0 (https://www.apache.org/licenses/LICENSE-2.0)
+// - Boost Software License, Version 1.0 (https://www.boost.org/LICENSE_1_0.txt)
+// - MIT License (https://mit-license.org/)
 // At your choosing (See accompanying files LICENSE_APACHE_2_0.txt,
 // LICENSE_MIT.txt and LICENSE_BOOST_1_0.txt).
 //
