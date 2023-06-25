6766622a93aa36f5c71de9bb5dff11ee1536b8fc
Mads Kiilerich <mads@kiilerich.com>
2023-06-25 18:55:26 +0200

    Rename license files to leave out version and .txt
    Just follow the convention used by many other rust packages.

--- src/unix.rs
+++ src/unix.rs
@@ -4,8 +4,8 @@
 // - Apache License, Version 2.0 (https://www.apache.org/licenses/LICENSE-2.0)
 // - Boost Software License, Version 1.0 (https://www.boost.org/LICENSE_1_0.txt)
 // - MIT License (https://mit-license.org/)
-// At your choosing (See accompanying files LICENSE_APACHE_2_0.txt,
-// LICENSE_MIT.txt and LICENSE_BOOST_1_0.txt).
+// At your choosing (See accompanying files LICENSE_APACHE,
+// LICENSE_MIT and LICENSE_BOOST).
 
 use std::{
     borrow::Cow,
