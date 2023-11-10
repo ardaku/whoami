9957d9b4ced3396cfe617bb9cb2b00b53db7d035
Jeron Aldaron Lau <aldaronlau@gmail.com>
2023-11-09 23:21:02 -0600

    Address `clippy::needless_doctest_main`
    https://rust-lang.github.io/rust-clippy/master/index.html#needless_doctest_main

--- src/lib.rs
+++ src/lib.rs
@@ -9,44 +9,42 @@
 //! return [`OsString`]):
 //!
 //! ```rust
-//! fn main() {
-//!     println!(
-//!         "User's Name            whoami::realname():    {}",
-//!         whoami::realname(),
-//!     );
-//!     println!(
-//!         "User's Username        whoami::username():    {}",
-//!         whoami::username(),
-//!     );
-//!     println!(
-//!         "User's Language        whoami::lang():        {:?}",
-//!         whoami::lang().collect::<Vec<String>>(),
-//!     );
-//!     println!(
-//!         "Device's Pretty Name   whoami::devicename():  {}",
-//!         whoami::devicename(),
-//!     );
-//!     println!(
-//!         "Device's Hostname      whoami::hostname():    {}",
-//!         whoami::hostname(),
-//!     );
-//!     println!(
-//!         "Device's Platform      whoami::platform():    {}",
-//!         whoami::platform(),
-//!     );
-//!     println!(
-//!         "Device's OS Distro     whoami::distro():      {}",
-//!         whoami::distro(),
-//!     );
-//!     println!(
-//!         "Device's Desktop Env.  whoami::desktop_env(): {}",
-//!         whoami::desktop_env(),
-//!     );
-//!     println!(
-//!         "Device's CPU Arch      whoami::arch():        {}",
-//!         whoami::arch(),
-//!     );
-//! }
+//! println!(
+//!     "User's Name            whoami::realname():    {}",
+//!     whoami::realname(),
+//! );
+//! println!(
+//!     "User's Username        whoami::username():    {}",
+//!     whoami::username(),
+//! );
+//! println!(
+//!     "User's Language        whoami::lang():        {:?}",
+//!     whoami::lang().collect::<Vec<String>>(),
+//! );
+//! println!(
+//!     "Device's Pretty Name   whoami::devicename():  {}",
+//!     whoami::devicename(),
+//! );
+//! println!(
+//!     "Device's Hostname      whoami::hostname():    {}",
+//!     whoami::hostname(),
+//! );
+//! println!(
+//!     "Device's Platform      whoami::platform():    {}",
+//!     whoami::platform(),
+//! );
+//! println!(
+//!     "Device's OS Distro     whoami::distro():      {}",
+//!     whoami::distro(),
+//! );
+//! println!(
+//!     "Device's Desktop Env.  whoami::desktop_env(): {}",
+//!     whoami::desktop_env(),
+//! );
+//! println!(
+//!     "Device's CPU Arch      whoami::arch():        {}",
+//!     whoami::arch(),
+//! );
 //! ```
 
 #![warn(
