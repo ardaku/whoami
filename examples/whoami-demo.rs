69745a86a5482496776a41de049bed61e6f30629
Jeron Aldaron Lau <aldaronlau@gmail.com>
2023-11-14 23:28:35 -0600

    Add `langs()` function, deprecate `lang()`
    Resolves #40

--- examples/whoami-demo.rs
+++ examples/whoami-demo.rs
@@ -3,38 +3,40 @@ fn main() {
     println!();
     println!(
         "User's Name            whoami::realname():    {}",
-        whoami::realname()
+        whoami::realname(),
     );
     println!(
         "User's Username        whoami::username():    {}",
-        whoami::username()
+        whoami::username(),
     );
     println!(
         "User's Language        whoami::lang():        {:?}",
-        whoami::lang().collect::<Vec<String>>(),
+        whoami::langs()
+            .map(|l| l.map(|l| l.to_string()).unwrap_or("??".to_string()))
+            .collect::<Vec<String>>(),
     );
     println!(
         "Device's Pretty Name   whoami::devicename():  {}",
-        whoami::devicename()
+        whoami::devicename(),
     );
     println!(
         "Device's Hostname      whoami::hostname():    {}",
-        whoami::hostname()
+        whoami::hostname(),
     );
     println!(
         "Device's Platform      whoami::platform():    {}",
-        whoami::platform()
+        whoami::platform(),
     );
     println!(
         "Device's OS Distro     whoami::distro():      {}",
-        whoami::distro()
+        whoami::distro(),
     );
     println!(
         "Device's Desktop Env.  whoami::desktop_env(): {}",
-        whoami::desktop_env()
+        whoami::desktop_env(),
     );
     println!(
         "Device's CPU Arch      whoami::arch():        {}",
-        whoami::arch()
+        whoami::arch(),
     );
 }
