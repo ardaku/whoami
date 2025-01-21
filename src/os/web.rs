3384de50791c8e92fccdae64b7b51b87b564d717
Robert Bacula <bacularobert@gmail.com>
2025-01-21 16:10:37 +0200

    improved web.rs error handling

--- src/os/web.rs
+++ src/os/web.rs
@@ -35,7 +35,10 @@ impl Target for Os {
                 .collect::<Vec<String>>()
                 .join(";"))
         } else {
-            Err(Error::new(ErrorKind::NotFound, "Window missing"))
+            Err(Error::new(
+                ErrorKind::NotFound,
+                "Failed to retrieve languages: Window object is missing",
+            ))
         }
     }
 
@@ -91,7 +94,12 @@ impl Target for Os {
     fn hostname(self) -> Result<String> {
         document_domain()
             .filter(|x| !x.is_empty())
-            .ok_or_else(|| Error::new(ErrorKind::NotFound, "Domain missing"))
+            .ok_or_else(|| {
+                Error::new(
+                    ErrorKind::NotFound,
+                    "Domain missing, failed to retrieve document domain from window"
+                )
+            })
     }
 
     fn distro(self) -> Result<String> {
@@ -169,12 +177,18 @@ impl Target for Os {
         let begin = if let Some(b) = string.find('(') {
             b
         } else {
-            return Platform::Unknown("Failed to parse platform: Missing '(' in user agent".to_string());
+            return Platform::Unknown(
+                "Failed to parse platform: Missing '(' in user agent"
+                    .to_string(),
+            );
         };
         let end = if let Some(e) = string.find(')') {
             e
         } else {
-            return Platform::Unknown("Failed to parse platform: Missing ')' in user agent".to_string());
+            return Platform::Unknown(
+                "Failed to parse platform: Missing ')' in user agent"
+                    .to_string(),
+            );
         };
         let string = &string[begin + 1..end];
 
