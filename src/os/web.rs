39b1fef306a77e2ccccc7c2b37a75db93599c4ab
Jeryn Aldaron Lau <aldaronlau@gmail.com>
2025-03-07 01:09:32 -0600

    Remove some TODOs now that dedicated issues exist (#177)

--- src/os/web.rs
+++ src/os/web.rs
@@ -157,15 +157,6 @@ impl Target for Os {
                 string[begin..].to_string().replace('_', ".")
             }
         } else {
-            // TODO:
-            // Platform::FreeBsd,
-            // Platform::Ios,
-            // Platform::Android,
-            // Platform::Nintendo3s,
-            // Platform::PlayStation,
-            // Platform::Dive,
-            // Platform::Fuchsia,
-            // Platform::Redox
             string.to_string()
         })
     }
@@ -196,15 +187,6 @@ impl Target for Os {
         } else if string.contains("Mac OS X") {
             Platform::Mac
         } else {
-            // TODO:
-            // Platform::FreeBsd,
-            // Platform::Ios,
-            // Platform::Android,
-            // Platform::Nintendo3ds,
-            // Platform::PlayStation,
-            // Platform::Dive,
-            // Platform::Fuchsia,
-            // Platform::Redox,
             Platform::Unknown(string.to_string())
         }
     }
